5f2a022deda41a397e1d68d7620e2b857c118d3f
Fidan <xelilovafidan61@gmail.com>
2025-10-20 01:18:30 +0400

    login

--- src/components/LoginSlider.tsx
+++ src/components/LoginSlider.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Mousewheel, Pagination } from 'swiper/modules'; // ✅ Add Pagination
+import { Mousewheel, Pagination } from 'swiper/modules'; 
 import 'swiper/css';
-import 'swiper/css/pagination'; // ✅ Import pagination styles
+import 'swiper/css/pagination'; 
 import styles from './LoginSlider.module.scss';
 import group from '../assets/Group 254.png';
 
@@ -18,11 +18,10 @@ export default function LoginSlider() {
       onSwiper={(swiper) => console.log(swiper)}
       className={styles.slider}
     >
-      <SwiperSlide>
+      <SwiperSlide className='slide'>
         <img src={group} alt="group" className={styles.image} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={group} alt="group" className={styles.image} />
+        <p className={styles.title}>Xoş gəlmişsiniz</p>
+        <p className={styles.body}>Azərpoçt-da şəxsi kabinet yaratmaqla siz sürətli ödənişlər və köçürmələr edə, xərclərinizə və gəlirlərinizə nəzarət edə bilərsiniz</p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={group} alt="group" className={styles.image} />
