05e5c5599437728dd14ef14bd2dd9fffbbdfb45f
Fidan <xelilovafidan61@gmail.com>
2025-10-22 11:14:11 +0400

    nav and footer componenets

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -1,32 +1,33 @@
 import { Button, Flex } from 'antd';
-import logo from '../assets/Azərpoçt AZ horizontal 1.png'
 import styles from './Password.module.scss'
 import { Input } from 'antd';
+import info from '../assets/fi_info.png';
+import AuthNav from './AuthNav';
+import AuthFooter from './AuthFooter';
+
 
 const PasswordRecoveryPage = () => {
   return (
     <div className={styles.PasswordContainer}>
 
-      <div className={styles.nav}>
-        <img src={logo} alt="Azərpoçt logo" />
-        <div>
-          <p>AZ</p>
-          <p>EN</p>
-          <p>RU</p>
-        </div>
+      <div>
+        <AuthNav/>
       </div>
 
       <div className={styles.login}>
         <p>Şifrənin bərpası</p>
-        <Input placeholder="İstifadəçi adı və ya e-mail" />
+        <Input placeholder="İstifadəçi adı və ya e-mail" className={styles.input}/>
+        <div>
+          <img src={info} alt="info" />
+          <p>Sizin email ünvanınıza şifrənin bərpasının təsdiqlənməsi barədə məktub göndəriləcək</p>
+        </div>
           <Flex gap="small" wrap>
-            <Button type="primary">Primary Button</Button>
+            <Button type="primary" className={styles.button}>Dəvam et</Button>
           </Flex>
       </div>
 
-      <div className={styles.footer}>
-        <p>Gizlilik Siyasəti</p>
-        <p>Azərpoçt 2022</p>
+      <div>
+        <AuthFooter/>
       </div>
 
     </div>
