3eba960894063620367fe660b36ef57ee8bfeb82
Fidan <xelilovafidan61@gmail.com>
2025-10-23 11:32:25 +0400

    button color

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'antd/dist/reset.css';
+import './components/reset.css'
 
 
 createRoot(document.getElementById('root')!).render(
