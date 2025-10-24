a974aec06bdd0b6eeda791381e3b11f5ef9735bf
Fidan <xelilovafidan61@gmail.com>
2025-10-24 13:01:30 +0400

    opt checking page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import PasswordRecoveryPage from './pages/PasswordRecoveryPage'
+import OTPPassowordCheck from './pages/OTPPassowordCheck'
 
 function App() {
 
@@ -8,7 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Register/>}/>  
-        <Route path='/password-recovery' element={<PasswordRecoveryPage/>}/> 
+        <Route path='/password-recovery' element={<PasswordRecoveryPage/>}/>
+        <Route path='/otp-checking' element={<OTPPassowordCheck/>}/>
       </Routes>
     </Router>
   )
