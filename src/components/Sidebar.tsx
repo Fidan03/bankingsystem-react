d218237cfb8b189244b97e7dd10def1661502f45
Fidan <xelilovafidan61@gmail.com>
2025-10-27 13:03:25 +0400

    Add Sidebar component to Homepage for improved layout

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,69 @@
+import home from '../assets/u_home-alt.png'
+import file from '../assets/file-text.png'
+import elementPlus from '../assets/element-plus.png'
+import card from '../assets/card.png'
+import receipt from '../assets/receipt-tax.png'
+import moneyBag from '../assets/money-bag (3) 1.png'
+import money from '../assets/money.png'
+import exchange from '../assets/exchange.png'
+import star from '../assets/star.png'
+import bookmark from '../assets/Bookmark.png'
+import logo from '../assets/azerpoct-az-hor.png'
+import styles from './Sidebar.module.scss'
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebarContainer}>
+        <div className={styles.logoContainer}>
+            <img src={logo} alt="logo" className={styles.logo}/>
+        </div>
+        <br/>
+        <div className={styles.menuList}>
+            <ul className={styles.list}>
+                <li className={styles.eachItem}>
+                    <img src={home} alt="home" className={styles.menuImg}/>
+                    <p className={styles.menuTitle}>Ana səhifə</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={file} alt="file-text" />
+                    <p>Ödəniş tapşırıqları</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={elementPlus} alt="elementPlus" />
+                    <p>Ödəniş sistemləri</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={card} alt="card" />
+                    <p>Kartlar və hesablar</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={receipt} alt="receipt" />
+                    <p>Kreditlər</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={moneyBag} alt="moneyBag" />
+                    <p>Əmanətlər</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={money} alt="money" />
+                    <p>Əmək haqqı layihəsi</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={exchange} alt="exchange" />
+                    <p>Valyuta mübadiləsi</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={star} alt="star" />
+                    <p>Ani Ödəniş sistemi</p>
+                </li>
+                <li className={styles.eachItem}>
+                    <img src={bookmark} alt="bookmark" />
+                    <p>Şablonlar</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Sidebar from '../components/Sidebar'
+
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Sidebar/>
+    </div>
   )
 }
 
