8bef769116f034b55acec8e33fb48cc3bf2a167b
Fidan <xelilovafidan61@gmail.com>
2025-10-30 10:47:17 +0400

    Add styling for HomepageMain component and implement file upload functionality with Ant Design's Upload component

--- src/components/HomepageMain.tsx
+++ src/components/HomepageMain.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { Select, Space, Input, Button, Flex } from 'antd';
-// import styles from './HomepageMain.module.scss'
+import { Select, Space, Input, Button, Flex, message, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+import styles from './HomepageMain.module.scss'
 
 const handleChange = (value: string) => {
   console.log(`selected ${value}`);
 };
 
+const props: UploadProps = {
+  name: 'file',
+  action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
 const HomepageMain: React.FC = () => (
 
-    <div>
+    <div className={styles.homepageMainContainer}>
 
-        <div>
+        <div className={styles.input}>
             <Space wrap>
                 <Select
                 defaultValue="lucy"
@@ -54,9 +74,34 @@ const HomepageMain: React.FC = () => (
         <div>
             <Flex gap="small" wrap>
                 <Button type="primary">Primary Button</Button>
+
                 <Button type="primary">Primary Button</Button>
             </Flex>
         </div>
+
+        <div>
+            <div>
+                <Upload {...props}>
+                    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                </Upload>
+            </div>
+
+            <div>
+                <div>
+                    <p>Köçürülən məbləğ</p>
+                    <p>5.20 AZN</p>
+                </div>
+
+                <p>Kommisiya: 0.00 AZN</p>
+            </div>
+
+            <div>
+                <Upload {...props}>
+                    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                </Upload>
+            </div>
+        </div>
+
     </div>
   
 );
