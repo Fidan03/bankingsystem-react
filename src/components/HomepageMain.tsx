9dc262a90cea621c70b241a064e025d3a3516533
Fidan <xelilovafidan61@gmail.com>
2025-10-30 10:27:19 +0400

    Refactor HomepageMain component to include Input fields and Buttons, enhancing layout with Flex and Space for better user interaction

--- src/components/HomepageMain.tsx
+++ src/components/HomepageMain.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select, Space } from 'antd';
+import { Select, Space, Input, Button, Flex } from 'antd';
 // import styles from './HomepageMain.module.scss'
 
 const handleChange = (value: string) => {
@@ -7,41 +7,58 @@ const handleChange = (value: string) => {
 };
 
 const HomepageMain: React.FC = () => (
-  <Space wrap>
-    <Select
-      defaultValue="lucy"
-      style={{ width: 120 }}
-      onChange={handleChange}
-      options={[
-        { value: 'jack', label: 'Jack' },
-        { value: 'lucy', label: 'Lucy' },
-        { value: 'Yiminghe', label: 'yiminghe' },
-        { value: 'disabled', label: 'Disabled', disabled: true },
-      ]}
-    />
-        <Select
-      defaultValue="lucy"
-      style={{ width: 120 }}
-      onChange={handleChange}
-      options={[
-        { value: 'jack', label: 'Jack' },
-        { value: 'lucy', label: 'Lucy' },
-        { value: 'Yiminghe', label: 'yiminghe' },
-        { value: 'disabled', label: 'Disabled', disabled: true },
-      ]}
-    />
-        <Select
-      defaultValue="lucy"
-      style={{ width: 120 }}
-      onChange={handleChange}
-      options={[
-        { value: 'jack', label: 'Jack' },
-        { value: 'lucy', label: 'Lucy' },
-        { value: 'Yiminghe', label: 'yiminghe' },
-        { value: 'disabled', label: 'Disabled', disabled: true },
-      ]}
-    />
-  </Space>
+
+    <div>
+
+        <div>
+            <Space wrap>
+                <Select
+                defaultValue="lucy"
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                    { value: 'jack', label: 'Jack' },
+                    { value: 'lucy', label: 'Lucy' },
+                    { value: 'Yiminghe', label: 'yiminghe' },
+                    { value: 'disabled', label: 'Disabled', disabled: true },
+                ]}
+                />
+                    <Select
+                defaultValue="lucy"
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                    { value: 'jack', label: 'Jack' },
+                    { value: 'lucy', label: 'Lucy' },
+                    { value: 'Yiminghe', label: 'yiminghe' },
+                    { value: 'disabled', label: 'Disabled', disabled: true },
+                ]}
+                />
+                    <Select
+                defaultValue="lucy"
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                    { value: 'jack', label: 'Jack' },
+                    { value: 'lucy', label: 'Lucy' },
+                    { value: 'Yiminghe', label: 'yiminghe' },
+                    { value: 'disabled', label: 'Disabled', disabled: true },
+                ]}
+                />
+            </Space>
+
+            <Input placeholder="Basic usage" />
+            <Input placeholder="Basic usage" />
+        </div>
+
+        <div>
+            <Flex gap="small" wrap>
+                <Button type="primary">Primary Button</Button>
+                <Button type="primary">Primary Button</Button>
+            </Flex>
+        </div>
+    </div>
+  
 );
 
 export default HomepageMain;
